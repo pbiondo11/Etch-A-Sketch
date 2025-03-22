7d957bff26fccdbdd9006b700c19231b4b92ee0e
pbiondo <biondo.a.peter@gmail.com>
2025-03-22 15:47:33 -0400

    Etch A Sketch Project Completed!

--- js.js
+++ js.js
@@ -1,44 +1,62 @@
-
 const gridContainer = document.getElementById('grid-container');
-
-for (let i = 0; i < 16; i++) {
-
-    const row = document.createElement('div');
-    row.classList.add('row');
-    gridContainer.appendChild(row);
-
-  for (let j = 0; j < 16; j++) {
-   
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    row.appendChild(gridItem);
-  }
-}
-
 const button = document.querySelector('button');
-button.addEventListener('click', ()=> {
 
-let size = prompt("Enter grid size:");
 
-if (size > 100) {
-alert("Enter a value that is less than or equal to 100");
-size = 0;
-let size = prompt("Enter grid size:");
-}
+function createGrid(size) {
+  
+  gridContainer.innerHTML = '';
 
-gridContainer.replaceChildren();
-
-for (let i = 0; i < size; i++) {
-
+  for (let i = 0; i < size; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     gridContainer.appendChild(row);
 
-  for (let j = 0; j < size; j++) {
-   
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    row.appendChild(gridItem);
+    for (let j = 0; j < size; j++) {
+      const gridItem = document.createElement('div');
+      gridItem.classList.add('grid-item');
+    
+      gridItem.dataset.opacity = 0;
+      row.appendChild(gridItem);
+
+     
+      gridItem.addEventListener('mouseover', () => {
+        setBgColor(gridItem);
+      });
+    }
   }
 }
+
+
+function setBgColor(gridItem) {
+
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  gridItem.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
+
+  let opacity = parseFloat(gridItem.dataset.opacity);
+
+
+  opacity = Math.min(opacity + 0.1, 1);
+
+  
+  gridItem.style.opacity = opacity;
+  gridItem.dataset.opacity = opacity;
+}
+
+
+createGrid(16);
+
+
+button.addEventListener('click', () => {
+  let size = prompt("Enter grid size:");
+
+  if (size > 100) {
+    alert("Enter a value that is less than or equal to 100");
+    size = prompt("Enter grid size:");
+  }
+
+
+  createGrid(parseInt(size));
 });
